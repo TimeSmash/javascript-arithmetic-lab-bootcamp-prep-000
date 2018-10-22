611b6d4d6c2aded072e8b15c9cdaee5bece7c597
mcumm64 <mcumm64@gmail.com>
2018-10-22 23:49:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function dec(n) {
   return (n = n - 1);
 }
 function makeInt(n) {
-  parseInt("n", 10)
+  return parseInt(n, 10)
 }
 function preserveDecimal(n) {
-  parseFloat("n", 10)
+  return parseFloat(n, 10)
 }
 
